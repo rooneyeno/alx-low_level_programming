58f566d2831ca364c50218ffc1cb399fc88aad8f
remmysilver <silverremmy@gmail.com>
2022-10-18 04:36:47 -0700

    all tasks done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - prints putchar
+ *Description: prints  putchar
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char  text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_abs - computes th absolute value of an integer
+ *@n: n is an integer
+ *Return: integer
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+
+	else if (n < 0)
+	{
+		return (-n);
+	}
+
+	else
+	{
+		return (n);
+	}
+}
