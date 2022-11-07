8974c8906931e819410511937890a11655fe8136
remmysilver <silverremmy@gmail.com>
2022-11-07 08:09:05 -0800

    lib

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: pointer to put the constan
+ * @b: constan
+ * @n: max bytes to us
+ * Return: 
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; n > 0 ; i++, n--)
+{
+	s[i] = b;
+}
+return (s);
+}
