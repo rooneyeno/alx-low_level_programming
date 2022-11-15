2975129e2f5cae562448f5104d8a16e7bd6b3c6a
remmysilver <silverremmy@gmail.com>
2022-11-15 06:53:48 -0800

    correction on 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,10 +2,11 @@
 #define OBJECT_LIKE_MACRO_
 
 /*
- *  *File: 0-object_like_macro.h
- *   *Desc: Header file that defines a macro named
- *    *SIZE as an abbreviation for the token 1024.
+ *File: 0-object_like_macro.h
+ *Desc: Header file that defines a macro named
+ *SIZE as an abbreviation for the token 1024.
  */
+
 #define SIZE 1024
 
 #endif
