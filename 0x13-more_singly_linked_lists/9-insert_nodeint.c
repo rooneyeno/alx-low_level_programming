711d976e2161d13a6be65327505bfbb8b8668320
remmysilver <silverremmy@gmail.com>
2022-11-30 23:04:02 -0800

    ok

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new = malloc(sizeof(listint_t));
 
-	if (!new || !head
+	if (!new || !head)
 		return (NULL);
 
 	new->n = n;
@@ -43,5 +43,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 	}
 	return (NULL);
-
 }
